6af7e71d051d1d97ab3010c26804ef6bcb8c53d5
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2021-04-03 14:41:27 -0500

    Create Client registration request

--- ClientManagement/Data/IClientDao.cs
+++ ClientManagement/Data/IClientDao.cs
@@ -7,12 +7,12 @@ namespace AppointmentManagerApi.Data
 {
     public interface IClientDao
     {
-        public GetClientResponse AddClient(ClientModel client);
+        public void AddClient(GetClientResponse client);
         public GetClientResponse AddClientFavorite(string clientUid, string professionalUid);
         public GetClientResponse GetClient(string uid);
         public IList<GetClientFavoritesResponse> GetClientFavorites(string uid);
         public IList<GetClientAppointmentsResponse> GetClientAppointments(string uid);
-        public GetClientResponse UpdateClient(ClientModel client);
+        public void UpdateClient(GetClientResponse client);
         public bool RemoveClient(string uid);
         public GetClientResponse RemoveClientFavorite(string clientUid, string professionalUid);
     }
